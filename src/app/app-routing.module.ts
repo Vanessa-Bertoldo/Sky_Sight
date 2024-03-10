b50c39b9b53e1c3dada93266cfd022ff239d134d
vanessa <vanessa.bert311@gmail.com>
2024-03-10 14:01:37 -0300

    insert component home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/skysight/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', //quando o caminho for vazio
+    redirectTo: 'skysight',
+    pathMatch: 'full'
+  },
+  {
+    path: 'skysight',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
